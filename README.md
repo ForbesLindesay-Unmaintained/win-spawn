2d8793e9fccdeba57c4aac163279be227b55a42f
ForbesLindesay <forbes@lindesay.co.uk>
2013-01-31 10:49:53 +0000

    Support environment variables and command line operation

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-win-spawn
-=========
+# win-spawn
 
-Spawn for node.js but in a way that works regardless of which OS you're using.  Use this if you want to use spawn with a JavaScript file.  It works by explicitly invoking node on windows.
+  Spawn for node.js but in a way that works regardless of which OS you're using.  Use this if you want to use spawn with a JavaScript file.  It works by explicitly invoking node on windows.  It also shims support for environment variable setting by attempting to parse the command with a regex.  Since all modification is wrapped in `if (os === 'Windows_NT')` it can be safely used on non-windows systems and will not break anything.
+
+## Installation
+
+  $ npm install win-spawn
+
+## Usage
+
+### Command Line
+
+  All the following will work exactly as if the 'win-spawn ' prefix was ommitted when on unix.
+
+    $ win-spawn foo
+    $ win-spawn ./bin/foo
+    $ win-spawn NODE_PATH=./lib foo
+    $ win-spawn NODE_PATH=./lib foo arg1 arg2
+
+### API
+
+This will just pass through to `child_process.spawn` on unix systems, but will correctly parse the arguments on windows.
+
+```javascript
+spawn('foo', [], {stdio: 'inherit'});
+spawn('./bin/foo', [], {stdio: 'inherit'});
+spawn('NODE_PATH=./lib foo', [], {stdio: 'inherit'});
+spawn('NODE_PATH=./lib foo', [arg1, arg2], {stdio: 'inherit'});
+```
--- index.js
+++ index.js
@@ -3,9 +3,46 @@ var os = require('os').type();
 
 module.exports = spawn;
 function spawn(command, args, options) {
-    if (os === 'Windows_NT') {
-        args = [command].concat(args);
-        command = process.execPath;
+  if (os === 'Windows_NT') {    
+    args = args || [];
+    options = options || {};
+    var match, matchA;
+    if (matchA = /((?:[A-Z_]+\=[^ \=]+ )+)?([^\r\n]+)/.exec(command)) {
+      try {
+        var file = require('fs').readFileSync(matchA[2], 'utf8');
+        if (match = /\#\!\/usr\/bin\/env ([^\r\n]+)/.exec(file)) {
+          args.unshift(matchA[2]);
+          command = matchA[1] + match[1];
+        }
+      } catch (ex) { }
     }
-    return cSpawn(command, args, options);
+
+    if (match = /((?:[A-Z_]+\=[^ \=]+ )+)([^\r\n]+)/.exec(command)) {
+      command = match[2];
+
+      options.env = options.env || shallowClone(process.env);
+
+      var env = match[1].split(' ');
+      env.forEach(function (v) {
+        v = v.split('=');
+        if (v.length === 2) {
+          options.env[v[0]] = v[1];
+        }
+      });
+    }
+
+    args.unshift(command);
+    args.unshift('/c');
+    command = 'cmd';
+  }
+  return cSpawn(command, args, options);
+}
+
+function shallowClone(obj) {
+  var out = {};
+  Object.keys(obj)
+    .forEach(function (key) {
+      out[key] = obj[key];
+    });
+  return out;
 }
